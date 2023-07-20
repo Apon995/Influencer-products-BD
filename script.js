93f695e4360dcdfd415c4cc14786b0cb7f44cb42
apon995 <shofikulsamz995@gmail.com>
2023-07-20 19:27:21 +0600

    first-commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const sideBarshow = () => {
+  document.querySelector(".side-bar").style.left = "0px";
+};
+
+const sideBargo = () => {
+  document.querySelector(".side-bar").style.left = "-127px";
+};
